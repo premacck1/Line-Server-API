1dd0b654ce554b164b26cddb87405a8665d7657d
Prem Ankur <premankur06@gmail.com>
2018-03-13 02:34:39 -0400

    More unit test cases included

--- src/test/java/com/server/LineServiceControllerTest.java
+++ src/test/java/com/server/LineServiceControllerTest.java
@@ -2,8 +2,13 @@ package com.server;
 
 import static org.junit.Assert.assertTrue;
 import java.io.*;
-import org.junit.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import org.junit.*;
+/*
+ * DB and Input File is mocked for testing. So this class is testing both Pre-processing and LineServiceController.
+ */
 
 public class LineServiceControllerTest {
 	static LineServiceController lsObj = null;
@@ -11,8 +16,8 @@ public class LineServiceControllerTest {
 	static String filePath;
 	static File f;
 
-	@Before
-	public void init() {
+	@BeforeClass
+	public static void init() {
 		Constants.STR_DB_PATH = "db_test";
 		preObj = new Preprocessor();
 		filePath = "test_file.txt";
@@ -34,24 +39,71 @@ public class LineServiceControllerTest {
 		}
 	}
 
-	@After
-	public void cleanup() {
+	@AfterClass
+	public static void cleanup() {
 		boolean b = f.delete();
 		System.out.println("Deleting test file: " + b);
+		lsObj.cache.clear();
 		Constants.STR_DB_PATH = "db2";
 		DbInstance.closeDB();
-		lsObj.cache.clear();
+		try {
+			Files.deleteIfExists(Paths.get("db_test"));
+		} catch (IOException e) {
+			System.out.println("Failed to delete db_test file. Please remove manually.");
+		}
 	}
 
 	@Test
-	public void testGetLineFromLineNumberForValidLine() {
+	public void testGetLineFromLineNumberForValidLine1() {
+		String output = lsObj.getLineFromLineNumber(50, filePath);
+		assertTrue(output.trim().equals("50 hey"));
+	}
+	
+	@Test
+	public void testGetLineFromLineNumberForValidLine2() {
+		String output = lsObj.getLineFromLineNumber(19, filePath);
+		assertTrue(output.trim().equals("19 hey"));
+	}
+	
+	@Test
+	public void testGetLineFromLineNumberForValidLine3() {
+		String output = lsObj.getLineFromLineNumber(77, filePath);
+		assertTrue(output.trim().equals("77 hey"));
+	}
+	
+	@Test
+	public void testGetLineFromLineNumberForValidLine4() {
+		String output = lsObj.getLineFromLineNumber(100, filePath);
+		assertTrue(output.trim().equals("100 hey"));
+	}
+	
+	@Test
+	public void testGetLineFromLineNumberForValidLine5() {
+		String output = lsObj.getLineFromLineNumber(1, filePath);
+		assertTrue(output.trim().equals("1 hey"));
+	}
+	
+	@Test
+	public void testGetLineFromLineNumberForValidLine6() {
 		String output = lsObj.getLineFromLineNumber(50, filePath);
 		assertTrue(output.trim().equals("50 hey"));
 	}
 
 	@Test
-	public void testGetLineFromLineNumberForInvalidLine() {
+	public void testGetLineFromLineNumberForInvalidLine1() {
 		String output = lsObj.getLineFromLineNumber(101, filePath);
+		assertTrue(output.equals(""));
+	}
+	
+	@Test
+	public void testGetLineFromLineNumberForInvalidLine2() {
+		String output = lsObj.getLineFromLineNumber(0, filePath);
+		assertTrue(output.equals(""));
+	}
+	
+	@Test
+	public void testGetLineFromLineNumberForInvalidLine3() {
+		String output = lsObj.getLineFromLineNumber(-2, filePath);
 		assertTrue(output.equals(""));
 	}
 
